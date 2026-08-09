1fca0377f89351d188ac9c04cf780d3f42f2faa6
srihariravisundar-ui <srihariravisundar@gmail.com>
2026-08-09 18:59:10 +0530

    Delete Editor directory

--- Editor/src/types/environment.d.ts
+++
@@ -1,5 +0,0 @@
-namespace NodeJS {
-  interface ProcessEnv extends NodeJS.ProcessEnv {
-    NEXT_PUBLIC_ACTIVE_NETWORK: '1' | '5';
-  }
-}
--- Editor/src/utils/common.ts
+++
@@ -1,8 +0,0 @@
-export function sleep(ms: number) {
-  return new Promise<void>((resolve) => setTimeout(resolve, ms));
-}
-
-export function getErrorMessage(error: unknown) {
-  if (error instanceof Error) return error.message;
-  return String(error);
-}
